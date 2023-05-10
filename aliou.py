08d566512c0c17ae3bbcd363a627388733fa1a25
aliou <seydinalioune192@gmail.com>
2023-05-10 13:04:31 +0200

    ajout de la table

--- aliou.py
+++ aliou.py
@@ -6,4 +6,10 @@ code qui permet de creer la fonction cos, sin, tan, arcth
 x = mat.cos(2)
 y  = mat.sin(x*x + 2 )
 print(" all done")
-print("Merci")
+
+
+
+
+x=int(input("Donner un nombre"))
+for i in range(1,11):
+    print(x,"*", i ","=",x*i)
